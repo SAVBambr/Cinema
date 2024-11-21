d096581e305377787c8091ed334c2064c87d6d80
SAVBambr <147664092+SAVBambr@users.noreply.github.com>
2024-11-21 16:14:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Обработчик для кнопки
+    document.getElementById("catalogButton").addEventListener("click", function() {
+        window.location.href = "каталог.html";
+    });
+
+});
